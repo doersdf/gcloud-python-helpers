a7f8d10de2229d92d77a3b6842742bd9e8ba861a
Antonio <antonio@doersdf.com>
2017-01-26 12:56:34 +0100

    added google sql server name parameter

--- cloud-sql/gcloud-sql-add-authorized-ip.py
+++ cloud-sql/gcloud-sql-add-authorized-ip.py
@@ -1,8 +1,8 @@
 # *********************
 # Script to authorize add an IP addres to sql authorized network
-# Usage: 'gcloud-sql-add-authorized-ip [instanceName] [command: add|remove] [IP-CIDR-notation]'
+# Usage: 'gcloud-sql-add-authorized-ip [command: add|remove] [IP-CIDR-notation]'
 # about CIDR notation https://en.wikipedia.org/wiki/Classless_Inter-Domain_Routing#CIDR_notation
-# Sample: 'gcloud-sql-add-authorized-ip instanceName add 10.10.10.10/24'
+# Sample: 'gcloud-sql-add-authorized-ip add 10.10.10.10/24 sql-server-name'
 # Author: Doers DF (hola@doersdf.com)
 # ********************
 
@@ -14,17 +14,17 @@ pattern = re.compile("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([
 
 
 if len(sys.argv)==4:
-    instanceName = sys.argv[3]
     newIP = sys.argv[2]
     command = sys.argv[1]
+    serversql = sys.argv[3]
     if pattern.match(newIP)==None:
-        print("[203] Error: Invalid IP.\n   Use IP CIDR notation Sample 'gcloud-sql-add-authorized-ip instanceName add 192.168.0.1/24'' ")
+        print("[203] Error: Invalid IP.\n   Use IP CIDR notation Sample 'gcloud-sql-add-authorized-ip add 192.168.0.1/24 sql-server-name'' ")
     elif command!="add" and command!="remove":
-        print("[202] Error: Invalid command.\n   Use Add or Remove 'gcloud-sql-add-authorized-ip instanceName add 192.168.0.1/24'' ")
+        print("[202] Error: Invalid command.\n   Use Add or Remove 'gcloud-sql-add-authorized-ip add 192.168.0.1/24 sql-server-name'' ")
     else: 
         #call google cloud sql api to get configuration in json format
         
-        p = subprocess.Popen("gcloud sql instances describe " + instanceName + " --format json", shell=True, stdout=subprocess.PIPE)
+        p = subprocess.Popen("gcloud sql instances describe " + serversql + " --format json", shell=True, stdout=subprocess.PIPE)
         p.wait()
 
         out, err = p.communicate()
@@ -45,7 +45,7 @@ if len(sys.argv)==4:
                 ips = newIP + ips
             else:
                 ips = ips[1:]
-            p = subprocess.Popen("gcloud sql instances patch " + instanceName + " --authorized-networks=" + ips, shell=True, stdout=subprocess.PIPE)
+            p = subprocess.Popen("gcloud sql instances patch " + serversql + " --authorized-networks=" + ips, shell=True, stdout=subprocess.PIPE)
             p.wait()
             print("[100] OK. Current authorizedNetworks = " + ips)
         else:
@@ -55,5 +55,5 @@ if len(sys.argv)==4:
                 print("[200] Warning: Especified IP not found. Nothing done.")
               
 else:
-    print("[201] Error: 3 argument required.\n   Usage 'gcloud-sql-add-authorized-ip.py yourInstanceName add 192.168.0.1/24 ")
+    print("[201] Error: 3 argument required.\n   Usage 'gcloud-sql-add-authorized-ip.py add 192.168.0.1/24 server-name")
 
